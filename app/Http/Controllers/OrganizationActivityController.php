bc915fd70d202a0c5a6b85887523bba53c9528e0
ihxnnxs <crashermoney0@gmail.com>
2025-08-01 18:00:50 +0300

    Реализована структура базы данных

---
+++ app/Http/Controllers/OrganizationActivityController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrganizationActivityController extends Controller
+{
+    //
+}
---
+++ app/Models/organizationPhone.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class organizationPhone extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrganizationPhoneFactory> */
+    use HasFactory;
+}
